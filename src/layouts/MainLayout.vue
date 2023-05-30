be3faaf515f3e3b01eb0c485703ea6e017008abd
jadsow <p.jadsonn@gmail.com>
2023-05-29 22:27:18 -0300

    ajustes - correção footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,56 +10,62 @@
         </router-view>
       </q-page-container>
     </div>
-    <q-footer :class="$q.screen.gt.sm? 'flex justify-end items-center bg-blue-grey-5' : 'flex justify-center items-center bg-blue-grey-5'">
+    <q-footer
+      :class="
+        $q.screen.gt.sm
+          ? 'flex justify-end items-center bg-blue-grey-5'
+          : 'flex justify-center items-center bg-blue-grey-5'
+      "
+    >
       <span class="text-body1">Desenvolvido por Jadson Pereira &copy;</span>
       <div class="flex">
         <q-item
-        clickable
-        @click="openURL('https://wa.me/5548984176989?text=Ol%C3%A1%21')"
-      >
-        <q-avatar size="sm">
-          <q-img src="../assets/icons/whatsapp.png"></q-img>
-        </q-avatar>
-      </q-item>
-      <q-item clickable @click="openURL('https://github.com/jadsow')">
-        <q-avatar size="sm">
-          <q-img src="../assets/icons/github.png"></q-img>
-        </q-avatar>
-      </q-item>
-      <q-item
-        clickable
-        @click="openURL('https://www.facebook.com/jadson.mariaeduarda')"
-      >
-        <q-avatar size="sm">
-          <q-img src="../assets/icons/facebook.png"></q-img>
-        </q-avatar>
-      </q-item>
-      <q-item
-        clickable
-        @click="openURL('https://www.instagram.com/p.jadsonn/')"
-      >
-        <q-avatar size="sm">
-          <q-img src="../assets/icons/instagram.png"></q-img>
-        </q-avatar>
-      </q-item>
-      <q-item
-        clickable
-        @click="
-          openURL('https://www.linkedin.com/in/jadson-pereira-a64421191/')
-        "
-      >
-        <q-avatar size="sm">
-          <q-img src="../assets/icons/linkedin.png"></q-img>
-        </q-avatar>
-      </q-item>
+          clickable
+          @click="openURL('https://wa.me/5548984176989?text=Ol%C3%A1%21')"
+        >
+          <q-avatar size="sm">
+            <q-img src="../assets/icons/whatsapp.png"></q-img>
+          </q-avatar>
+        </q-item>
+        <q-item clickable @click="openURL('https://github.com/jadsow')">
+          <q-avatar size="sm">
+            <q-img src="../assets/icons/github.png"></q-img>
+          </q-avatar>
+        </q-item>
+        <q-item
+          clickable
+          @click="openURL('https://www.facebook.com/jadson.mariaeduarda')"
+        >
+          <q-avatar size="sm">
+            <q-img src="../assets/icons/facebook.png"></q-img>
+          </q-avatar>
+        </q-item>
+        <q-item
+          clickable
+          @click="openURL('https://www.instagram.com/p.jadsonn/')"
+        >
+          <q-avatar size="sm">
+            <q-img src="../assets/icons/instagram.png"></q-img>
+          </q-avatar>
+        </q-item>
+        <q-item
+          clickable
+          @click="
+            openURL('https://www.linkedin.com/in/jadson-pereira-a64421191/')
+          "
+        >
+          <q-avatar size="sm">
+            <q-img src="../assets/icons/linkedin.png"></q-img>
+          </q-avatar>
+        </q-item>
       </div>
-
     </q-footer>
   </q-layout>
 </template>
 
 <script setup>
 import HeaderLayout from "./HeaderLayout.vue";
+import { openURL } from "quasar";
 
 import "animate.css";
 </script>
@@ -77,7 +83,4 @@ import "animate.css";
   font-family: "Cormorant Garamond", serif;
   color: white;
 }
-
-
-
 </style>
--- src/pages/Home/IndexHome.vue
+++ src/pages/Home/IndexHome.vue
@@ -2,29 +2,52 @@
   <q-page class="flex column">
     <div class="row q-col-gutter-sm">
       <div class="col-12 flex justify-center q-mt-md">
-        <span style="font-size: 30px; text-decoration: underline" class="text-bold">Histórico Profissional</span>
+        <span
+          style="font-size: 30px; text-decoration: underline"
+          class="text-bold"
+          >Histórico Profissional</span
+        >
       </div>
-      <div v-for="i in historico" :key="i" class="q-mt-md flex column text-center col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <div
+        v-for="i in historico"
+        :key="i"
+        class="q-mt-md flex column text-center col-xs-12 col-sm-12 col-md-12 col-lg-12"
+      >
         <span class="text-bold" style="font-size: 2em"> {{ i.empresa }} </span>
-        <span style="font-size: 20px;"> {{ i.funcao }} </span>
+        <span style="font-size: 20px"> {{ i.funcao }} </span>
       </div>
     </div>
   </q-page>
 </template>
 <script setup>
-
 const historico = [
-  {empresa: 'Panteu Sistemas - Fev/22 - Atual', funcao: 'Desenvolvedor front-end (Vue.JS e Quasar Framework)'},
-  {empresa: 'FAPESC - Fundação de Amparo à Pesquisa e Inovação do Estado de Santa Catarina', funcao: 'Coordenador de projetos de inovação'},
-  {empresa: 'SANTUR - Agência de Desenvolvimento do Turismo de Santa Catarina', funcao: 'Apoio administrativo junto à Gerência de Inteligência de dados'},
-  {empresa: 'FAPESC - Fundação de Amparo à Pesquisa e Inovação do Estado de Santa Catarina', funcao: 'Estágio no setor de Gerência e Inovação'},
-  {empresa: 'Oi Telecom - 2017 - 2017', funcao: 'Técnico ADSL - Reparos, instalações e configurações do acesso a internet'},
-  {empresa: 'Produza SA - 2013', funcao: 'Estágio na área de testes de placas eletrônicas'},
-]
-
+  {
+    empresa: "Panteu Sistemas - Fev/22 - Atual",
+    funcao: "Desenvolvedor front-end (Vue.JS e Quasar Framework)",
+  },
+  {
+    empresa:
+      "FAPESC - Fundação de Amparo à Pesquisa e Inovação do Estado de Santa Catarina",
+    funcao: "Coordenador de projetos de inovação",
+  },
+  {
+    empresa: "SANTUR - Agência de Desenvolvimento do Turismo de Santa Catarina",
+    funcao: "Apoio administrativo junto à Gerência de Inteligência de dados",
+  },
+  {
+    empresa:
+      "FAPESC - Fundação de Amparo à Pesquisa e Inovação do Estado de Santa Catarina",
+    funcao: "Estágio no setor de Gerência e Inovação",
+  },
+  {
+    empresa: "Oi Telecom - 2017 - 2019",
+    funcao:
+      "Técnico ADSL - Reparos, instalações e configurações do acesso a internet",
+  },
+  {
+    empresa: "Produza SA - 2013",
+    funcao: "Estágio na área de testes de placas eletrônicas",
+  },
+];
 </script>
-<style>
-
-
-
-</style>
+<style></style>
