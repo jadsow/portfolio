4b22529b9b6edf5527e0b1f9dff9a47130d717d9
Jadson <Jadson@BOOK-E1HMUGLQHJ>
2024-04-04 14:49:43 -0300

    Ajustes index

--- src/pages/IndexPortfolio.vue
+++ src/pages/IndexPortfolio.vue
@@ -5,10 +5,10 @@
     </p>
     <div class="flex justify-center">
       <ul>
-        <li>Vue.js;</li>
-        <li>VueRouter para roteamento;</li>
+        <li>Vue.js como framework;</li>
+        <li>VueRouter para o trabalho com as rotas do projeto;</li>
         <li>Pinia como gerenciador de estado;</li>
-        <li>Quasar Framework para estilização</li>
+        <li>Quasar Framework para estilização;</li>
         <li>Axios para consulta de API's;</li>
         <li>
           Utilização dos conceitos de componentização, composables e script
