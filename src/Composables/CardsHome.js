6e0cdb3d30816726b2306cbb9d23facd24ce7b14
Jadson <Jadson@BOOK-E1HMUGLQHJ>
2024-04-06 15:57:38 -0300

    ajustes cards home

--- src/Composables/CardsHome.js
+++ src/Composables/CardsHome.js
@@ -15,7 +15,7 @@ export const cardSize = () => {
     } else if ($q.screen.lg) {
       return "min-height: 450px";
     } else {
-      return ""; // Retorne um valor padrão se nenhum caso for atendido
+      return "min-height: 450px"; // Retorne um valor padrão se nenhum caso for atendido
     }
   });
 
